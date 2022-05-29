c2810d9539fd4824f8fe026ad7fac5dac5c234fb
matheusgoes23 <matheusgoes23@gmail.com>
2022-05-29 10:44:16 -0300

    fix. findByTema

--- src/main/java/br/edu/ufersa/sicon_quespro/repository/QuestaoRepository.java
+++ src/main/java/br/edu/ufersa/sicon_quespro/repository/QuestaoRepository.java
@@ -1,11 +1,13 @@
 package br.edu.ufersa.sicon_quespro.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import br.edu.ufersa.sicon_quespro.model.Questao;
 import br.edu.ufersa.sicon_quespro.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface QuestaoRepository extends JpaRepository<Questao, Long> {	
-	public Questao findByTema(Tema tema);
+public interface QuestaoRepository extends JpaRepository<Questao, Long> {
+
+    @Query("SELECT obj FROM Questao obj WHERE obj.temas = :tema")
+    Questao findByTema(Tema tema);
 
 }
